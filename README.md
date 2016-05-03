c79baf00b47df1593a4962863292d05d9f07c195
Adrian <adrian.she@alumni.ubc.ca>
2016-05-02 17:31:14 -0700

    Experiments from May 2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # coding-theory-usra
-# coding-theory-usra
+
+Code snippets for project
---
+++ two_by_n_matrix.m
@@ -0,0 +1,21 @@
+%% Construct the matrix A representing the recursion
+A = [1 1 1; 1 0 1; 1 1 0];
+
+%% Compute C_n: the number of 2 x n matrices
+%% with no adjacent ones in each row or each column
+vec = [1; 1; 1];
+count = zeros(10, 1);
+count(1) = sum(vec);
+
+for i=2:20
+    vec = A * vec;
+    count(i) = sum(vec);
+endfor
+
+fprintf("Number of 2 x n matrices with no adjacent ones \n");
+count
+fprintf("Theoretical Capacity \n");
+log2(1+sqrt(2)) %% Theoretical Capacity
+fprintf("Emperically Determined Capacity \n");
+polyfit((1:20)', log2(count), 1)(1) %% Emperically determined capacity
+
