48fbbf8638f6c854acd4f3e805446c8cefa09fa8
Adrian <adrian.she@alumni.ubc.ca>
2016-05-02 17:33:49 -0700

    Fix weird stuff

--- three_by_n_matrix.m
+++ three_by_n_matrix.m
@@ -13,12 +13,12 @@ for i=2:20
     count(i) = sum(vec);
 endfor
 
-fprintf("Number of 3 x n matrices with no adjacent ones in rows or columns \n")
+fprintf('Number of 3 x n matrices with no adjacent ones in rows or columns \n')
 format long;
 count 
 
-fprintf("Estimate of Theoretical Capacity \n");
+fprintf('Estimate of Theoretical Capacity \n');
 log2(3.63138126040364)
 
-fprintf("Emperical Capacity \n");
+fprintf('Emperical Capacity \n');
 polyfit((1:20)', log2(count), 1)(1)
--- two_by_n_matrix.m
+++ two_by_n_matrix.m
@@ -12,10 +12,10 @@ for i=2:20
     count(i) = sum(vec);
 endfor
 
-fprintf("Number of 2 x n matrices with no adjacent ones \n");
+fprintf('Number of 2 x n matrices with no adjacent ones \n');
 count
-fprintf("Theoretical Capacity \n");
+fprintf('Theoretical Capacity \n');
 log2(1+sqrt(2)) %% Theoretical Capacity
-fprintf("Emperically Determined Capacity \n");
+fprintf('Emperically Determined Capacity \n');
 polyfit((1:20)', log2(count), 1)(1) %% Emperically determined capacity
 
