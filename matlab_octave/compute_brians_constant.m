a9301e3276b7cfecdb383257920f799f6dbb8266
Adrian <adrian.she@alumni.ubc.ca>
2016-06-06 15:34:40 -0700

    Add linear system stuff

--- matlab_octave/compute_brians_constant.m
+++ matlab_octave/compute_brians_constant.m
@@ -1,11 +1,11 @@
-probabilities = reshape(1:32000, 20, 40, 40);
+probabilities = reshape(1:8000, 5, 40, 40);
 
-for a=1:20
+for a=15:20
    T1 = generate_matrix_numrep(a);
    T2 = generate_matrix_numrep(a+1);
    E = generate_expander_matrix(a);
    printf('Finishing generating matrix \n');
    fflush(stdout);
    vals = compute_all_constants(T1, T2, E, a, 40, 41);
-   probabilities(a,:,:) = vals;
+   probabilities(a - 14,:,:) = vals;
 endfor
