d14e6f831ac9aa7029c2c57a2a2173413e77be34
Arman Raina <arman.raina@yahoo.com>
2016-07-18 12:40:35 -0700

    faster rhombus

--- c/best/rhombus.cpp
+++ c/best/rhombus.cpp
@@ -17,6 +17,8 @@
 #include <bitset>
 #include <fstream>
 #include <string>     // std::string, std::to_string
+#include <assert.h>     /* assert */
+#include "time.h"
 
 
 using namespace std;
@@ -217,6 +219,20 @@ Eigen::VectorXd multiply_z_matrix_e(int m, std::vector<long> seqs, std::vector<d
     return ret;
 }
 
+Eigen::VectorXd multiply_z_matrix_eb(int n, int m, std::vector<long> seqs, std::vector<double> z_counts, Eigen::VectorXd vec){
+    int size = seqs.size();
+    Eigen::VectorXd ret(size);
+    ret.fill(0);
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            if (is_compatible(seqs[i], seqs[j])){
+                if (is_end_m(seqs[j], m) && is_bot(seqs[j],n)) ret(j)+= z_counts[j]*vec(i);
+            }
+        }
+    }
+    return ret;
+}
+
 Eigen::MatrixXd generate_z_matrix_helper(std::vector<long> seqs, double z, std::vector<int> counts){
     int size = seqs.size();
     std::vector<double> z_counts;
@@ -309,6 +325,7 @@ double probability_rhombus_imp_omit(int a, double z, int n, std::vector<int> cou
             a=a-2;
         }
     }
+    
     // remove middle
     if (n > 0){ 
         if (a>0 && (a<(3*n+3-2*((n+1)/2 +1) ))){
@@ -323,6 +340,7 @@ double probability_rhombus_imp_omit(int a, double z, int n, std::vector<int> cou
             vec = multiply_z_matrix_tb(n, seqs, z_counts,vec);
         }
     }
+
     // remove after middle
     for (int j = (n+1)/2+2; j <= n; j++){
         if ((a>0) && (a<(3*n+3-2*j))){
@@ -339,16 +357,126 @@ double probability_rhombus_imp_omit(int a, double z, int n, std::vector<int> cou
             a=a-2;
         }
     }
-
+    
     // remove final
      if (n > 1){ 
         int m = n+1;
         if (a<=(n+1)) m = a;
         init = multiply_z_matrix_e(m, seqs, z_counts, init);
         vec = multiply_z_matrix_e(m, seqs, z_counts, vec);
-        a-=m;
+        a -= m;
+
     }
    return init.sum()/vec.sum();
+}
+
+std::vector<double> probability_rhombus_imp_omit_bulk(double z, int n, std::vector<int> counts, std::vector<long> seqs){
+    assert(n>1);
+    std::vector<double> out;
+    out.resize(4*n+1);
+ 
+    Eigen::VectorXd init = generate_init(z, seqs, counts);
+    Eigen::VectorXd mid;
+    std::vector<double> z_counts;
+    int size = counts.size();
+
+    z_counts.resize(size); 
+    for (int k = 0; k<counts.size(); k++){
+        z_counts[k] = pow(z, counts[k]);
+    }
+     // compute full
+    Eigen::VectorXd vec = init; 
+
+    for (int i = 2; i <= (n+1)/2; i++){
+        vec = multiply_z_matrix(seqs, z_counts,vec);
+    }
+
+    mid = multiply_z_matrix_c(n, seqs, z_counts, vec);
+    vec = multiply_z_matrix(seqs, z_counts,vec);
+
+    for (int j = (n+1)/2+2; j <= n; j++){
+        mid = multiply_z_matrix(seqs, z_counts, mid);
+        vec = multiply_z_matrix(seqs, z_counts, vec);
+    }
+    // add full
+    for (int a = 0; a <=n+1; a++){
+        Eigen::VectorXd mid_2 = multiply_z_matrix_e(a, seqs, z_counts, mid);
+        Eigen::VectorXd vec_2 = multiply_z_matrix_e(a, seqs, z_counts, vec);
+        out[a] = mid_2.sum()/vec_2.sum();
+    }
+
+     // compute top
+    init = generate_init_m(1, z, seqs, counts);  
+    vec = init; 
+
+    for (int i = 2; i <= (n+1)/2; i++){
+        vec = multiply_z_matrix_t(seqs, z_counts,vec);
+    }
+
+    mid = multiply_z_matrix_ct(n, seqs, z_counts, vec);
+    vec = multiply_z_matrix_t(seqs, z_counts,vec);
+
+    for (int j = (n+1)/2+2; j <= n; j++){
+        mid = multiply_z_matrix_t(seqs, z_counts, mid);
+        vec = multiply_z_matrix_t(seqs, z_counts, vec);
+    }
+    // add top
+    for (int a = 2; a <=n+1;a++){
+        Eigen::VectorXd mid_2 = multiply_z_matrix_e(a, seqs, z_counts, mid);
+        Eigen::VectorXd vec_2 = multiply_z_matrix_e(a, seqs, z_counts, vec);
+        out[n+a] = mid_2.sum()/vec_2.sum();
+    }
+
+     // compute init
+   
+    init = generate_init_m(n+1, z, seqs, counts); 
+    vec = init; 
+
+    for (int i = 2; i <= (n+1)/2; i++){
+        vec = multiply_z_matrix_t(seqs, z_counts,vec);
+    }
+    
+   mid = multiply_z_matrix_ct(n, seqs, z_counts, vec);
+    vec = multiply_z_matrix_t(seqs, z_counts,vec);
+    
+   for (int j = (n+1)/2+2; j <= n; j++){
+        mid = multiply_z_matrix_t(seqs, z_counts, mid);
+        vec = multiply_z_matrix_t(seqs, z_counts, vec);
+    }
+   // add init
+    for (int a = 2; a <=n+1;a++){ 
+        Eigen::VectorXd mid_2 = multiply_z_matrix_e(a, seqs, z_counts, mid);
+        Eigen::VectorXd vec_2 = multiply_z_matrix_e(a, seqs, z_counts, vec);
+        double prob =  mid_2.sum()/vec_2.sum();
+        out[2*n+a] = prob;
+        cout << 2*n + a << setw(30) << prob << endl;
+    }
+      // compute final
+   init = generate_init_m(n+1, z, seqs, counts); 
+    vec = init; 
+    
+    for (int i = 2; i <= (n+1)/2; i++){
+        vec = multiply_z_matrix_tb(n, seqs, z_counts,vec);
+    }
+    
+   mid = multiply_z_matrix_ctb(n, seqs, z_counts, vec);
+    vec = multiply_z_matrix_tb(n, seqs, z_counts,vec);
+
+   for (int j = (n+1)/2+2; j <= n; j++){
+        mid = multiply_z_matrix_tb(n, seqs, z_counts, mid);
+        vec = multiply_z_matrix_tb(n, seqs, z_counts, vec);
+    }
+    // add final
+   for (int a = 2; a <=n;a++){
+        Eigen::VectorXd mid_2 = multiply_z_matrix_eb(n,a, seqs, z_counts, mid);
+        Eigen::VectorXd vec_2 = multiply_z_matrix_eb(n,a, seqs, z_counts, vec);
+        double prob =  mid_2.sum()/vec_2.sum();
+        out[3*n+a] = prob;
+        cout << 3*n + a << setw(30) << prob << endl;
+    }
+       
+
+   return out;    
 }
 
 double probability_rhombus_imp(Eigen::VectorXd init, double z, int n, std::vector<int> counts, std::vector<long> seqs){
@@ -403,22 +531,23 @@ int main(int argc, char* argv[]){
 
 int main(int argc, char* argv[]){
     int n_end = 8;
-    int z_end = 4;
-    for (double z = 3; z <= z_end; z+=0.05){
+    double z_end = 4;
+    for (double z = 3; z<=z_end; z+=0.25){ 
         string name_n = std::to_string(z);
         ofstream n_output (string("rhombus_perc_z = " + name_n +".txt").c_str());
-        n_output << left << setw(5) << "n" << setw(5) << "a" << setw(30) << "z" << setw(30) << "prob" << endl;
-        cout << left << setw(5) << "n" << setw(5) << "a" << setw(30) << "z" << setw(30) << "prob" << endl;
-        for (int n = n_end; n >= 0; n--){
+        n_output << left << setw(5) << "n" << setw(5) << "a" << setw(30) << "z" << setw(30) << "prob_1" << endl;
+        cout     << left << setw(5) << "n" << setw(5) << "a" << setw(30) << "z" << setw(30) << "prob_1" << endl;
+        for (int n = 2; n <= n_end; n++){
             std::vector<long> seqs = generate_linear_one_dim_sequences(2*n+1);
             std::vector<int> counts = generate_counts(seqs);
-            for (int a = 0; a < 4*n; a++){
+           std::vector<double> ret = probability_rhombus_imp_omit_bulk(z,n,counts, seqs);
+           for (int a = 0; a <= 4*n; a++){
                 Eigen::VectorXd vec = generate_init(z,seqs, counts);
-                double prob_2 = probability_rhombus_imp_omit(a, z, n, counts, seqs);
-                cout << left << setw(5) << n << setw(5) << a << setw(30) << z << setw(30) << setprecision(16) << prob_2 << endl; 
-                n_output << left << setw(5) << n << setw(5) << a << setw(30) << z << setw(30) << setprecision(16) << prob_2 << endl; 
+                double prob_1 = ret[a];
+//                double prob_2 = probability_rhombus_imp_omit(a, z, n, counts, seqs);
+                cout     << left << setw(5) << n << setw(5) << a << setw(30) << z << setw(30) << setprecision(16) << prob_1 << endl; 
+                n_output << left << setw(5) << n << setw(5) << a << setw(30) << z << setw(30) << setprecision(16) << prob_1 << endl; 
             }
-        }
+       }
     }
 }
-
